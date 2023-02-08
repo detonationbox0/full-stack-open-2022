27587c53a6ce211a0122d3612b1118615d4ee6d1
detonationbox0 <detonationbox0@live.com>
2023-02-08 17:13:27 -0500

    Working on 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,10 +5,16 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
+  
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -32,9 +38,21 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
+
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name:
+          <input  value={newName}
+                  onChange={handleNameChange}
+          />
         </div>
+
+        <div>
+          number:
+          <input  value={newPhone}
+                  onChange={handlePhoneChange}
+          />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
@@ -47,7 +65,7 @@ const App = () => {
           )
         })} 
       </ul>
-      <div>debug: {newName}</div>
+      <div>debug: {newPhone}</div>
     </div>
   )
 }
